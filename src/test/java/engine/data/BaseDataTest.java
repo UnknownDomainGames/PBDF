a581a4fd510d0625a4fdee185f8635cb65e6d5b1
Mouse <mouse0w0@qq.com>
2020-03-28 21:54:42 +0800

    Add gzip test

--- src/test/java/engine/data/BaseDataTest.java
+++ src/test/java/engine/data/BaseDataTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 abstract class BaseDataTest {
 
@@ -14,6 +16,19 @@ abstract class BaseDataTest {
         DataUtils.write(element, new DataOutputStream(output));
         DataElement read = DataUtils.read(new DataInputStream(new ByteArrayInputStream(output.toByteArray())));
         Assertions.assertEquals(element, read);
+    }
+
+    @Test
+    public void testGzip() throws IOException {
+        DataElement element = create();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        try (GZIPOutputStream outputStream = new GZIPOutputStream(output)) {
+            DataUtils.write(element, new DataOutputStream(outputStream));
+        }
+        try (GZIPInputStream inputStream = new GZIPInputStream(new ByteArrayInputStream(output.toByteArray()))) {
+            DataElement read = DataUtils.read(new DataInputStream(inputStream));
+            Assertions.assertEquals(element, read);
+        }
     }
 
     protected abstract DataElement create();
